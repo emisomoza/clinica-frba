36fe63abe965d7943e2091408b3b8fccd030d3e1
emisomoza <emisomoza@hotmail.com>
2016-11-29 11:00:42 -0300

    Creo nuevamente el form de rol

---
+++ ClinicaFrba/AbmRol/ABM_Rol.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace ClinicaFrba.AbmRol
+{
+    partial class ABM_Rol
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lnkAgregar = new System.Windows.Forms.LinkLabel();
+            this.dgvRoles = new System.Windows.Forms.DataGridView();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblLimpiarCampos = new System.Windows.Forms.LinkLabel();
+            this.chkFuncionalidades = new System.Windows.Forms.CheckedListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cmbEstado = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNombreRol = new System.Windows.Forms.TextBox();
+            this.lNomRol = new System.Windows.Forms.Label();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnCrear = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRoles)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lnkAgregar
+            // 
+            this.lnkAgregar.AutoSize = true;
+            this.lnkAgregar.Location = new System.Drawing.Point(678, 479);
+            this.lnkAgregar.Name = "lnkAgregar";
+            this.lnkAgregar.Size = new System.Drawing.Size(44, 13);
+            this.lnkAgregar.TabIndex = 13;
+            this.lnkAgregar.TabStop = true;
+            this.lnkAgregar.Text = "Agregar";
+            // 
+            // dgvRoles
+            // 
+            this.dgvRoles.AllowUserToAddRows = false;
+            this.dgvRoles.AllowUserToDeleteRows = false;
+            this.dgvRoles.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvRoles.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnF2;
+            this.dgvRoles.Location = new System.Drawing.Point(12, 12);
+            this.dgvRoles.MultiSelect = false;
+            this.dgvRoles.Name = "dgvRoles";
+            this.dgvRoles.ReadOnly = true;
+            this.dgvRoles.RowTemplate.ReadOnly = true;
+            this.dgvRoles.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvRoles.ShowEditingIcon = false;
+            this.dgvRoles.Size = new System.Drawing.Size(339, 386);
+            this.dgvRoles.TabIndex = 12;
+            this.dgvRoles.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvRoles_CellContentClick);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(93, 404);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(82, 23);
+            this.btnCancelar.TabIndex = 11;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblLimpiarCampos);
+            this.groupBox1.Controls.Add(this.chkFuncionalidades);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.cmbEstado);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtNombreRol);
+            this.groupBox1.Controls.Add(this.lNomRol);
+            this.groupBox1.Location = new System.Drawing.Point(370, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(311, 386);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Complete los campos";
+            // 
+            // lblLimpiarCampos
+            // 
+            this.lblLimpiarCampos.AutoSize = true;
+            this.lblLimpiarCampos.Location = new System.Drawing.Point(6, 370);
+            this.lblLimpiarCampos.Name = "lblLimpiarCampos";
+            this.lblLimpiarCampos.Size = new System.Drawing.Size(81, 13);
+            this.lblLimpiarCampos.TabIndex = 228;
+            this.lblLimpiarCampos.TabStop = true;
+            this.lblLimpiarCampos.Text = "Limpiar Campos";
+            this.lblLimpiarCampos.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lblLimpiarCampos_LinkClicked);
+            // 
+            // chkFuncionalidades
+            // 
+            this.chkFuncionalidades.Location = new System.Drawing.Point(31, 76);
+            this.chkFuncionalidades.Name = "chkFuncionalidades";
+            this.chkFuncionalidades.ScrollAlwaysVisible = true;
+            this.chkFuncionalidades.Size = new System.Drawing.Size(235, 244);
+            this.chkFuncionalidades.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(28, 330);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Estado";
+            // 
+            // cmbEstado
+            // 
+            this.cmbEstado.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbEstado.FormattingEnabled = true;
+            this.cmbEstado.Location = new System.Drawing.Point(31, 346);
+            this.cmbEstado.Name = "cmbEstado";
+            this.cmbEstado.Size = new System.Drawing.Size(235, 21);
+            this.cmbEstado.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Funcionalidades";
+            // 
+            // txtNombreRol
+            // 
+            this.txtNombreRol.Location = new System.Drawing.Point(31, 38);
+            this.txtNombreRol.MaxLength = 255;
+            this.txtNombreRol.Name = "txtNombreRol";
+            this.txtNombreRol.Size = new System.Drawing.Size(235, 20);
+            this.txtNombreRol.TabIndex = 3;
+            // 
+            // lNomRol
+            // 
+            this.lNomRol.AutoSize = true;
+            this.lNomRol.Location = new System.Drawing.Point(28, 22);
+            this.lNomRol.Name = "lNomRol";
+            this.lNomRol.Size = new System.Drawing.Size(44, 13);
+            this.lNomRol.TabIndex = 2;
+            this.lNomRol.Text = "Nombre";
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Location = new System.Drawing.Point(181, 403);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(82, 24);
+            this.btnModificar.TabIndex = 225;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            this.btnModificar.Click += new System.EventHandler(this.btnModificar_Click);
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(12, 404);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 224;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // btnCrear
+            // 
+            this.btnCrear.Location = new System.Drawing.Point(269, 404);
+            this.btnCrear.Name = "btnCrear";
+            this.btnCrear.Size = new System.Drawing.Size(82, 23);
+            this.btnCrear.TabIndex = 227;
+            this.btnCrear.Text = "Crear";
+            this.btnCrear.UseVisualStyleBackColor = true;
+            this.btnCrear.Click += new System.EventHandler(this.btnCrear_Click);
+            // 
+            // cl_AbmRol
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(693, 462);
+            this.Controls.Add(this.btnCrear);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.lnkAgregar);
+            this.Controls.Add(this.dgvRoles);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "cl_AbmRol";
+            this.Text = "Administrar Roles";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRoles)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.LinkLabel lnkAgregar;
+        private System.Windows.Forms.DataGridView dgvRoles;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cmbEstado;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNombreRol;
+        private System.Windows.Forms.Label lNomRol;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnCrear;
+        private System.Windows.Forms.LinkLabel lblLimpiarCampos;
+        private System.Windows.Forms.CheckedListBox chkFuncionalidades;
+    }
+}
---
+++ ClinicaFrba/AbmRol/ABM_Rol.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClinicaFrba.AbmRol
+{
+    public partial class ABM_Rol : Form
+    {
+        public ABM_Rol()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void lblLimpiarCampos_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void validarForm()
+        {
+
+        }
+
+        private void dgvRoles_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void f_completar_funcionalidades()
+        {
+
+        }
+
+        private void btnCrear_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public static void getFuncionalidades()
+        {
+
+        }
+
+        public static void getRoles()
+        {
+
+        }
+
+        public static void getFuncRol(string nombreRol, CheckedListBox funcionalidades)
+        {
+
+        }
+
+        public static void crearRol(string nombreRol, int estado, CheckedListBox funcionalidades)
+        {
+
+        }
+
+        public static void modificarFunc(string nombreRol, string funcionalidad, Int32 habilitado)
+        {
+           
+        }
+        public static void modificarRol(string nombreRol, Int32 estado, CheckedListBox funcionalidades)
+        {
+            
+        }
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
