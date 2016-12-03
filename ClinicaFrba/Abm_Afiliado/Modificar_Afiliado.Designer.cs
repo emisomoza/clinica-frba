3d677e1f9cc48694c86d4019036fa1c0d86c0c4d
emisomoza <emisomoza@hotmail.com>
2016-12-03 20:48:21 -0300

    Modificar afiliado ok

---
+++ ClinicaFrba/Abm_Afiliado/Modificar_Afiliado.Designer.cs
@@ -0,0 +1,323 @@
+﻿namespace ClinicaFrba.Abm_Afiliado
+{
+    partial class Modificar_Afiliado
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupAltaAfiliado = new System.Windows.Forms.GroupBox();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cmbEstadoCivil = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtTelefono = new System.Windows.Forms.TextBox();
+            this.txtDireccion = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.linkCleanAfiliado = new System.Windows.Forms.LinkLabel();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.btnBuscarClie = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDocumento = new System.Windows.Forms.TextBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblApellido = new System.Windows.Forms.Label();
+            this.lblDocumento = new System.Windows.Forms.Label();
+            this.groupAltaAfiliado.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupAltaAfiliado
+            // 
+            this.groupAltaAfiliado.Controls.Add(this.lblDocumento);
+            this.groupAltaAfiliado.Controls.Add(this.lblApellido);
+            this.groupAltaAfiliado.Controls.Add(this.lblNombre);
+            this.groupAltaAfiliado.Controls.Add(this.btnGuardar);
+            this.groupAltaAfiliado.Controls.Add(this.label6);
+            this.groupAltaAfiliado.Controls.Add(this.label4);
+            this.groupAltaAfiliado.Controls.Add(this.cmbEstadoCivil);
+            this.groupAltaAfiliado.Controls.Add(this.label3);
+            this.groupAltaAfiliado.Controls.Add(this.txtTelefono);
+            this.groupAltaAfiliado.Controls.Add(this.txtDireccion);
+            this.groupAltaAfiliado.Controls.Add(this.label2);
+            this.groupAltaAfiliado.Controls.Add(this.label10);
+            this.groupAltaAfiliado.Controls.Add(this.label11);
+            this.groupAltaAfiliado.Controls.Add(this.linkCleanAfiliado);
+            this.groupAltaAfiliado.Controls.Add(this.txtEmail);
+            this.groupAltaAfiliado.Controls.Add(this.label12);
+            this.groupAltaAfiliado.Controls.Add(this.label13);
+            this.groupAltaAfiliado.Location = new System.Drawing.Point(21, 62);
+            this.groupAltaAfiliado.Name = "groupAltaAfiliado";
+            this.groupAltaAfiliado.Size = new System.Drawing.Size(721, 257);
+            this.groupAltaAfiliado.TabIndex = 18;
+            this.groupAltaAfiliado.TabStop = false;
+            this.groupAltaAfiliado.Text = "Modificación de afiliado";
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(623, 200);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(75, 23);
+            this.btnGuardar.TabIndex = 30;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(30, 181);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(61, 13);
+            this.label6.TabIndex = 25;
+            this.label6.Text = "Estado civil";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(30, 210);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 23;
+            // 
+            // cmbEstadoCivil
+            // 
+            this.cmbEstadoCivil.FormattingEnabled = true;
+            this.cmbEstadoCivil.Location = new System.Drawing.Point(151, 178);
+            this.cmbEstadoCivil.Name = "cmbEstadoCivil";
+            this.cmbEstadoCivil.Size = new System.Drawing.Size(151, 21);
+            this.cmbEstadoCivil.TabIndex = 22;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Teléfono";
+            // 
+            // txtTelefono
+            // 
+            this.txtTelefono.Location = new System.Drawing.Point(151, 126);
+            this.txtTelefono.Name = "txtTelefono";
+            this.txtTelefono.Size = new System.Drawing.Size(220, 20);
+            this.txtTelefono.TabIndex = 17;
+            // 
+            // txtDireccion
+            // 
+            this.txtDireccion.Location = new System.Drawing.Point(151, 100);
+            this.txtDireccion.Name = "txtDireccion";
+            this.txtDireccion.Size = new System.Drawing.Size(220, 20);
+            this.txtDireccion.TabIndex = 16;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Direccion";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(30, 21);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(47, 13);
+            this.label10.TabIndex = 2;
+            this.label10.Text = "Nombre:";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(30, 49);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(47, 13);
+            this.label11.TabIndex = 3;
+            this.label11.Text = "Apellido:";
+            // 
+            // linkCleanAfiliado
+            // 
+            this.linkCleanAfiliado.AutoSize = true;
+            this.linkCleanAfiliado.Location = new System.Drawing.Point(10, 241);
+            this.linkCleanAfiliado.Name = "linkCleanAfiliado";
+            this.linkCleanAfiliado.Size = new System.Drawing.Size(81, 13);
+            this.linkCleanAfiliado.TabIndex = 13;
+            this.linkCleanAfiliado.TabStop = true;
+            this.linkCleanAfiliado.Text = "Limpiar Campos";
+            this.linkCleanAfiliado.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.limpiarCampos_LinkClicked);
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(151, 152);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(220, 20);
+            this.txtEmail.TabIndex = 12;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(30, 77);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(118, 13);
+            this.label12.TabIndex = 0;
+            this.label12.Text = "Numero de documento:";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(30, 155);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(32, 13);
+            this.label13.TabIndex = 11;
+            this.label13.Text = "Email";
+            // 
+            // btnBuscarClie
+            // 
+            this.btnBuscarClie.Location = new System.Drawing.Point(0, 0);
+            this.btnBuscarClie.Name = "btnBuscarClie";
+            this.btnBuscarClie.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscarClie.TabIndex = 0;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(666, 325);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(85, 23);
+            this.btnCancelar.TabIndex = 9;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 13);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "Ingrese documento";
+            // 
+            // txtDocumento
+            // 
+            this.txtDocumento.Location = new System.Drawing.Point(131, 22);
+            this.txtDocumento.Name = "txtDocumento";
+            this.txtDocumento.Size = new System.Drawing.Size(100, 20);
+            this.txtDocumento.TabIndex = 20;
+            this.txtDocumento.TextChanged += new System.EventHandler(this.txtDocumento_TextChanged);
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(248, 22);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 21;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(80, 21);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(44, 13);
+            this.lblNombre.TabIndex = 22;
+            this.lblNombre.Text = "Nombre";
+            this.lblNombre.Visible = false;
+            // 
+            // lblApellido
+            // 
+            this.lblApellido.AutoSize = true;
+            this.lblApellido.Location = new System.Drawing.Point(80, 49);
+            this.lblApellido.Name = "lblApellido";
+            this.lblApellido.Size = new System.Drawing.Size(44, 13);
+            this.lblApellido.TabIndex = 31;
+            this.lblApellido.Text = "Apellido";
+            this.lblApellido.Visible = false;
+            // 
+            // lblDocumento
+            // 
+            this.lblDocumento.AutoSize = true;
+            this.lblDocumento.Location = new System.Drawing.Point(151, 77);
+            this.lblDocumento.Name = "lblDocumento";
+            this.lblDocumento.Size = new System.Drawing.Size(60, 13);
+            this.lblDocumento.TabIndex = 32;
+            this.lblDocumento.Text = "documento";
+            this.lblDocumento.Visible = false;
+            // 
+            // Modificar_Afiliado
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(763, 361);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.txtDocumento);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupAltaAfiliado);
+            this.Controls.Add(this.btnCancelar);
+            this.Name = "Modificar_Afiliado";
+            this.Text = "Modificación Afiliado";
+            this.groupAltaAfiliado.ResumeLayout(false);
+            this.groupAltaAfiliado.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.GroupBox groupAltaAfiliado;
+        private System.Windows.Forms.LinkLabel linkCleanAfiliado;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Button btnBuscarClie;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtTelefono;
+        private System.Windows.Forms.TextBox txtDireccion;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cmbEstadoCivil;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDocumento;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Label lblDocumento;
+        private System.Windows.Forms.Label lblApellido;
+        private System.Windows.Forms.Label lblNombre;
+    }
+}
---
+++ ClinicaFrba/Abm_Afiliado/Modificar_Afiliado.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClinicaFrba.DB_Connection;
+
+namespace ClinicaFrba.Abm_Afiliado
+{
+    public partial class Modificar_Afiliado : Form
+    {
+        public int id_afiliado { get; set; }
+        public Modificar_Afiliado()
+        {
+            InitializeComponent();
+            inicializarEstadosCiviles();
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            int parsedValue;
+
+            if (
+                string.IsNullOrWhiteSpace(txtEmail.Text) ||
+                string.IsNullOrWhiteSpace(txtDireccion.Text) ||
+                string.IsNullOrWhiteSpace(txtTelefono.Text)
+                )
+            {
+                MessageBox.Show("Completa todos los datos");
+            }
+            else if (!int.TryParse(txtTelefono.Text, out parsedValue))
+            {
+                MessageBox.Show("El teléfono debe contener solo números.");
+            }
+            else
+            {
+                SQL sql = new SQL();
+
+                List<Parametro> parametros = new List<Parametro>();
+
+                Parametro param_id_afiliado = new Parametro("id_afiliado", this.id_afiliado);
+                parametros.Add(param_id_afiliado);
+                Parametro id_estado_civil = new Parametro("id_estado_civil", cmbEstadoCivil.SelectedValue);
+                parametros.Add(id_estado_civil);
+                Parametro direccion = new Parametro("direccion", txtDireccion.Text);
+                parametros.Add(direccion);
+                Parametro telefono = new Parametro("telefono", txtTelefono.Text);
+                parametros.Add(telefono);
+                Parametro email = new Parametro("email", txtEmail.Text);
+                parametros.Add(email);
+
+                DataTable tabla = sql.ejecutarSP("usp_modifica_afiliado", parametros);
+                if (tabla.Rows.Count > 0 && tabla.Rows[0].ItemArray[0].ToString() == "ERROR")
+                {
+                    MessageBox.Show(tabla.Rows[0].ItemArray[1].ToString());
+                }
+                else
+                {
+                    MessageBox.Show(tabla.Rows[0].ItemArray[0].ToString());
+                }
+            }
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void inicializarEstadosCiviles()
+        {
+            SQL sql = new SQL();
+
+            DataTable tabla = sql.ejecutarSP("usp_get_estados_civiles");
+            if (tabla.Rows.Count > 0 && tabla.Rows[0].ItemArray[0].ToString() == "ERROR")
+            {
+                MessageBox.Show(tabla.Rows[0].ItemArray[1].ToString());
+            }
+            else
+            {
+                cmbEstadoCivil.DataSource = tabla;
+                cmbEstadoCivil.DisplayMember = "DESCRIPCION";
+                cmbEstadoCivil.ValueMember = "ID_ESTADO_CIVIL";
+            }
+        }
+
+        private void limpiarCampos_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            this.lblApellido.Visible = false;
+            this.lblNombre.Visible = false;
+            this.lblDocumento.Visible = false;
+            this.txtDireccion.Text = String.Empty;
+            this.txtEmail.Text = String.Empty;
+            this.txtTelefono.Text = String.Empty;
+            this.cmbEstadoCivil.SelectedIndex = 0;
+            this.txtDocumento.Text = String.Empty;
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            SQL sql = new SQL();
+            List<Parametro> parametros = new List<Parametro>();
+
+            Parametro documento_param = new Parametro("nro_documento", Convert.ToInt32(txtDocumento.Text));
+            parametros.Add(documento_param);
+
+            DataTable tabla = sql.ejecutarSP("usp_obtener_afiliados_x_documento_modif", parametros);
+            if (tabla.Rows.Count == 0)
+            {
+                MessageBox.Show("No se obtuvieron resultados con el documento ingresado");
+            }
+            else if (tabla.Rows.Count > 0 && tabla.Rows[0].ItemArray[0].ToString() == "ERROR")
+            {
+                MessageBox.Show(tabla.Rows[0].ItemArray[1].ToString());
+            }
+            else
+            {
+                id_afiliado = Convert.ToInt32(tabla.Rows[0].ItemArray[0]);
+                lblNombre.Visible = true;
+                lblApellido.Visible = true;
+                lblDocumento.Visible = true;
+                lblNombre.Text = tabla.Rows[0].ItemArray[1].ToString();
+                lblApellido.Text = tabla.Rows[0].ItemArray[2].ToString();
+                txtDireccion.Text = tabla.Rows[0].ItemArray[3].ToString();
+                txtTelefono.Text = tabla.Rows[0].ItemArray[4].ToString();
+                txtEmail.Text = tabla.Rows[0].ItemArray[5].ToString();
+                lblDocumento.Text = txtDocumento.Text;
+                cmbEstadoCivil.SelectedValue = tabla.Rows[0].ItemArray[6].ToString();
+            }
+        }
+
+        private void txtDocumento_TextChanged(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtDocumento.Text))
+            {
+                btnBuscar.Visible = true;
+            }
+        }
+    }
+}
